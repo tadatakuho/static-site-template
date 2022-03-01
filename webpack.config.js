74cf5e31dde7d827c50de29decce21b7e2067bfa
tadatakuho <tadatakuho@team-lab.com>
2022-03-01 12:23:59 +0900

    ビルド先のディレクトリ名をdocsに修正

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: "./src/index.ts",
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
   },
   mode: "development",
   plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
